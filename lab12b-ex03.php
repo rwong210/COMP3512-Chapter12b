1c0a6398f75bfa395088b05188b60f862274f4f8
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-11 12:33:47 -0700

    finished ex03

--- lab12b-ex03.php
+++ lab12b-ex03.php
@@ -1,39 +1,47 @@
 <?php
-
+$data = [
+    [10, 10],
+    [60, 60],
+    [110, 80],
+    [160, 20],
+    [210, 80],
+    [260, 120],
+    [310, 150],
+    [390, 110],
+    [440, 10],
+    [490, 190]
+];
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>  
-    <title>Lab 12b</title>   
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">    
+    <meta charset="utf-8" />
+    <title>Lab 12b</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="css/lab12b-ex03.css">
 </head>
+
 <body>
-<main class="container">
+    <main class="container">
 
-    <div class="box"> 
-        <h1>Stock History</h1>
-        <div class="data">
-            <svg viewBox="0 0 500 200" class="chart">
+        <div class="box">
+            <h1>Stock History</h1>
+            <div class="data">
+                <svg viewBox="0 0 500 200" class="chart">
 
-              <polyline
-                 points="
-                        10,10
-                        60,60
-                        110,80
-                        160,20
-                        210,80
-                        260,120
-                        310,150
-                        390,110
-                        440,10
-                        490,190
-                   "/>
-            </svg>
+                    <polyline points="
+                         <?php
+                            foreach ($data as $point) {
+                                echo $point[0] . ',' . $point[1] . ' ';
+                            }
+                            ?>
+                   " />
+                </svg>
+            </div>
         </div>
-    </div>
 
-</main>   
+    </main>
 </body>
+
 </html>
