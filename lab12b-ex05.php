f9b313a0fe64b8438f04c674ba5552f4c952d19d
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-11 13:52:10 -0700

    finished ex05

--- lab12b-ex05.php
+++ lab12b-ex05.php
@@ -1,28 +1,49 @@
 <?php
-   // add class definition here
+// add class definition here
+class Stock
+{
+   public $symbol;
+   public $name;
+   public $percent;
+   public $value;
 
+   function __construct($s, $n, $p, $v)
+   {
+      $this->symbol = $s;
+      $this->name = $n;
+      $this->percent = $p;
+      $this->value = $v;
+   }
+}
 
+$foo = new Stock("AMZN", "Amazon.com Inc.", 25, 1760.19);
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>  
-    <title>Lab 12b</title>   
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">    
-    <link rel="stylesheet" href="css/lab12b-ex04.css">
+   <meta charset="utf-8" />
+   <title>Lab 12b</title>
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
+   <link rel="stylesheet" href="css/lab12b-ex04.css">
 </head>
+
 <body>
-<main class="container">
+   <main class="container">
 
-    <div class="box"> 
-      <h1>Portfolio Overview (Using Objects)</h1>
-      <div class="data">
-         <?php
-            // test out object here
-         ?>
+      <div class="box">
+         <h1>Portfolio Overview (Using Objects)</h1>
+         <div class="data">
+            <?php
+            echo "<h2>$foo->symbol</h2>";
+            echo "<h3>$foo->name</h3>";
+            echo "<p>$foo->percent% at $" . $foo->value . "</p>";
+            ?>
+         </div>
       </div>
-    </div>
 
-</main>   
+   </main>
 </body>
+
 </html>
