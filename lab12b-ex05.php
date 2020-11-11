a703f9eb5bb8453322d45073fe300d91f04fece6
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-11 14:25:17 -0700

    finished ex06

--- lab12b-ex05.php
+++ lab12b-ex05.php
@@ -14,7 +14,22 @@ class Stock
       $this->percent = $p;
       $this->value = $v;
    }
+
+   public function __toString()
+   {
+      $s = "<h2>$$this->symbol</h2>";
+      $s .= "<h3>$this->name</h3>";
+      $s .= "<p>$this->percent% at $" . $this->value . "</p>";
+      return $s;
+   }
 }
+
+$portfolio = [
+   new Stock("AMZN", "Amazon.ca Inc.", 25, 1760.19),
+   new Stock("APPL", "Apple Inc.", 20, 218.32),
+   new Stock("MSFT", "Microsoft Corporation", 15, 108.82),
+   new Stock("GOOGL", "Alphabet Inc.", 40, 1111.39)
+];
 
 $foo = new Stock("AMZN", "Amazon.com Inc.", 25, 1760.19);
 ?>
@@ -36,9 +51,19 @@ $foo = new Stock("AMZN", "Amazon.com Inc.", 25, 1760.19);
          <h1>Portfolio Overview (Using Objects)</h1>
          <div class="data">
             <?php
-            echo "<h2>$foo->symbol</h2>";
-            echo "<h3>$foo->name</h3>";
-            echo "<p>$foo->percent% at $" . $foo->value . "</p>";
+            //echo "<h2>$foo->symbol</h2>";
+            //echo "<h3>$foo->name</h3>";
+            //echo "<p>$foo->percent% at $" . $foo->value . "</p>";
+            // foreach ($portfolio as $p) {
+            // echo "<h2>$p->symbol</h2>";
+            // echo "<h3>$p->name</h3>";
+            // echo "<p>$p->percent% at $" . $foo->value . "</p>";
+            // }
+
+            foreach ($portfolio as $p) {
+               echo $p;
+            }
+
             ?>
          </div>
       </div>
