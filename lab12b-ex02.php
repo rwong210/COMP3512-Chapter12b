8deaeebd501645c40577f3768017f596da05c07e
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-11 11:30:17 -0700

    finished ex02

--- lab12b-ex02.php
+++ lab12b-ex02.php
@@ -1,25 +1,34 @@
 <?php
 
-$stocks = ["AMZN","GOOGL","APPL","MSFT","FB"];
+$stocks = ["AMZN", "GOOGL", "APPL", "MSFT", "FB"];
 
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8"/>  
-    <title>Lab 12b</title>   
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">    
-    <link rel="stylesheet" href="css/lab12b-ex02.css">
-</head>
-<body>
-<main class="container">
 
-    <div class="box"> 
-		<h1>Portfolio Overview</h1>
-		<div class="data">
-		</div>
+<head>
+  <meta charset="utf-8" />
+  <title>Lab 12b</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
+  <link rel="stylesheet" href="css/lab12b-ex02.css">
+</head>
+
+<body>
+  <main class="container">
+
+    <div class="box">
+      <h1>Portfolio Overview</h1>
+      <div class="data">
+        <?php
+        sort($stocks);
+        foreach ($stocks as $s) {
+          echo $s . '<br>';
+        }
+        ?>
+      </div>
     </div>
 
-</main>   
+  </main>
 </body>
+
 </html>
