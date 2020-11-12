2ac783bb394a75a521fe6b6a13af392d9bbf7bd6
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-12 10:27:18 -0700

    fixed format test01

--- lab12b-test01.php
+++ lab12b-test01.php
@@ -5,20 +5,23 @@ include('includes/lab12b-test01.inc.php');
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8"/>  
-    <title>Lab 12b</title>   
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">   
+    <meta charset="utf-8" />
+    <title>Lab 12b</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
     <link rel="stylesheet" href="css/weather-icons.min.css">
-        
+
     <link rel="stylesheet" href="css/lab12b-test01.css">
 </head>
-<body>
-<main class="grid-container">
 
-   <?php
+<body>
+    <main class="grid-container">
+
+        <?php
         cityBox($weatherData);
-   ?>
-</main>   
+        ?>
+    </main>
 </body>
+
 </html>
