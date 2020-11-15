008f046b0c6d2d61e4fbf9b8d0ad2454568026f1
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-15 14:21:16 -0700

    finished ex10

--- lab12b-ex10.php
+++ lab12b-ex10.php
@@ -1,6 +1,26 @@
-<?php 
+<?php
 
-include 'includes/simple-data.inc.php'; 
+include 'includes/simple-data.inc.php';
 
+// check if query string exists, if it does return only the data for a single book
+$data = $books;
+if (isset($_GET['isbn'])) {
 
-?>
+    // check if the ISBN exists in our data
+    $found = null;
+    foreach ($books as $b) {
+        if ($b['isbn'] == $_GET['isbn']) {
+            $found = $b;
+        }
+    }
+    if (isset($found)) {
+        $data = $found;
+    } else {
+        header("Location: lab12b-error.php");
+    }
+}
+
+/* Content-Type http header is used to tell the browser what kind of content it is about to receive. 
+The json_encode() function converts a PHP associative array into JSON. */
+header('Content-Type: application/json');
+echo json_encode($data, JSON_NUMERIC_CHECK + JSON_PRETTY_PRINT);
