48ad7ce5a40105f342dc4e349f35f11c9bcd7891
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-15 18:54:08 -0700

    ex12 finished

--- lab12b-ex12.php
+++ lab12b-ex12.php
@@ -4,7 +4,14 @@
 
     $imgname = "images/art/0.jpg";
     $img = imagecreatefromjpeg($imgname);
+    $newimg = imagescale($img, 600, 600);
 
-    imagejpeg($img);
+    $fontFile = realpath('font/Lato-Medium.ttf');
+    $fontSize = 16;
+    $textColor = imagecolorallocate($newimg, 238, 238, 238);
+    imagettftext($newimg, $fontSize, 0, 250, 160, $textColor, $fontFile, "Anyone want a drink of this?");
+
+
+    imagejpeg($newimg);
 
 ?>
