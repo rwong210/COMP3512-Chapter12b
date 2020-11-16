21e8199871163142c0bbd956fcef774bcb6062fa
Ryan J. Wong <rwong210@mtroyal.ca>
2020-11-15 18:31:03 -0700

    finished ex11

--- lab12b-ex11.php
+++ lab12b-ex11.php
@@ -1,3 +1,21 @@
 <?php
 
-?>
+// tell browser that this is returning an image
+header('Content-Type: image/png');
+
+// create an image in with dimensions 500x500
+$img = imagecreatetruecolor(500, 500);
+
+// define the rgba color to fill the image with 
+$color = imagecolorallocate($img, 238, 71, 49);
+
+// use color to fill the image
+imagefilledrectangle($img, 0, 0, 499, 499, $color);
+
+$fontFile = realpath('font/Lato-Medium.ttf');
+$fontSize = 24;
+$textColor = imagecolorallocate($img, 238, 238, 238);
+imagettftext($img, $fontSize, 0, 50, 230, $textColor, $fontFile, "Something Witty");
+
+// output the image to the response stream (back to the browser)
+imagepng($img);
